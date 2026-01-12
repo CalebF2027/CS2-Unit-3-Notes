797e153154a073c430ecf41054fb31a265427609
caleb <caleb.fisher2027@gbwl.org>
2026-01-12 14:40:54 +0000

    3.8 notes

--- Main.java
+++ Main.java
@@ -10,13 +10,35 @@ public class Main {
 
       // All objects can be created using this pattern
       // ClassName objectName = new ClassName();
-      BuildABear kitty = new BuildABear();
+      BuildABear basicBear = new BuildABear();
       // "BuildABear kitty" is DECLARING a variable of type BuildABear
       // "new BuildABear()" is CALLING a CONSTRUCTOR to set up the object
 
       // Look at our new object
-      System.out.println(kitty);
+      System.out.println(basicBear);
+
+      // Create a new obkect wit our values
+      // called the CONSTRUCTOR: ClassName(String, String, int)
+      BuildABear puppy = new BuildABear("dog", "James", 90);
+
+      // Printing an OBJECT calls the CLASS' toString() method
+      System.out.println(puppy);
+
+      // Test our methods below by CALLING them on an object instance
+      // GETTERS are NON-VOID (we need to handle output data)
+      System.out.println( puppy.getType() );
+      // or, store the return value in a variable
+      double price = puppy.getPrice();
+      System.out.println(price);
+
+      // SETTERS are VOID (means no return value)
+      // Calling voud methods: objectName.methodName();
+
+      puppy.setName("James");
+      puppy.setFillLevel(90);
+      // setters (mutators) changes vaues. let's see the effects:
+      System.out.println(puppy);
 
 
-   }
-}
+   } // end main() METHOD
+} // end Main CLASS
